1f874178a5bb4a7b1dc6924851cc17f88d14ced0
gmackie <graham.mackie@gmail.com>
2020-01-20 01:12:44 -0500

    update stuff

--- apps-sdk/sdk.h
+++ apps-sdk/sdk.h
@@ -26,19 +26,22 @@
 // the various register offsets and values
 
 extern volatile uint32_t MISC[];
-#define MISC_REG(i) MISC[(i)/4]
+#define MISC_REG(i) MISC[(i) / 4]
+
+extern volatile uint32_t I2C[];
+#define I2C_REG(i) I2C[(i) / 4]
 
 extern volatile uint32_t UART;
-#define UART_REG(i) UART[(i)/4]
+#define UART_REG(i) UART[(i) / 4]
 
 extern volatile uint32_t LCD;
-#define LCD_REG(i) LCD[(i)/4]
+#define LCD_REG(i) LCD[(i) / 4]
 
 extern volatile uint32_t USB;
-#define USB_REG(i) USB[(i)/4]
+#define USB_REG(i) USB[(i) / 4]
 
 extern volatile uint32_t GFXREG[];
-#define GFX_REG(i) GFXREG[(i)/4]
+#define GFX_REG(i) GFXREG[(i) / 4]
 
 extern uint32_t GFXPAL[];
 extern uint32_t GFXTILES[];
--- soc/boot/ipl_flash.c
+++ soc/boot/ipl_flash.c
@@ -16,25 +16,35 @@
 #include "gloss/mach_defines.h"
 #include "ipl_flash.h"
 
-volatile uint32_t *MISC=(volatile uint32_t*)MISC_OFFSET;
-#define MISC_REG(i) MISC[(i/4)]
+volatile uint32_t *MISC = (volatile uint32_t *)MISC_OFFSET;
+#define MISC_REG(i) MISC[(i / 4)]
 
-static inline void flash_start_xfer(int flash_sel) {
-	if (flash_sel!=FLASH_SEL_CURRENT) {
-		MISC_REG(MISC_FLASH_SEL_REG)=(flash_sel==0)?MISC_FLASH_SEL_INTFLASH:MISC_FLASH_SEL_CARTFLASH;
+volatile uint32_t *I2C = (volatile uint32_t *)I2C_OFFSET;
+#define I2C_REG(i) I2C[(i / 4)]
+
+static inline void flash_start_xfer(int flash_sel)
+{
+	if (flash_sel != FLASH_SEL_CURRENT)
+	{
+		MISC_REG(MISC_FLASH_SEL_REG) = (flash_sel == 0) ? MISC_FLASH_SEL_INTFLASH : MISC_FLASH_SEL_CARTFLASH;
 	}
-	MISC_REG(MISC_FLASH_CTL_REG)=MISC_FLASH_CTL_CLAIM;
+	MISC_REG(MISC_FLASH_CTL_REG) = MISC_FLASH_CTL_CLAIM;
 }
 
-static inline void flash_end_xfer() {
-	while (!(MISC_REG(MISC_FLASH_CTL_REG)&MISC_FLASH_CTL_IDLE));
-	MISC_REG(MISC_FLASH_CTL_REG)=0;
+static inline void flash_end_xfer()
+{
+	while (!(MISC_REG(MISC_FLASH_CTL_REG) & MISC_FLASH_CTL_IDLE))
+		;
+	MISC_REG(MISC_FLASH_CTL_REG) = 0;
 }
 
-static inline uint8_t flash_send_recv(uint8_t data) {
-	while (!(MISC_REG(MISC_FLASH_CTL_REG)&MISC_FLASH_CTL_IDLE));
-	MISC_REG(MISC_FLASH_WDATA_REG)=data;
-	while (!(MISC_REG(MISC_FLASH_CTL_REG)&MISC_FLASH_CTL_IDLE));
+static inline uint8_t flash_send_recv(uint8_t data)
+{
+	while (!(MISC_REG(MISC_FLASH_CTL_REG) & MISC_FLASH_CTL_IDLE))
+		;
+	MISC_REG(MISC_FLASH_WDATA_REG) = data;
+	while (!(MISC_REG(MISC_FLASH_CTL_REG) & MISC_FLASH_CTL_IDLE))
+		;
 	return MISC_REG(MISC_FLASH_RDATA_REG);
 }
 
@@ -57,86 +67,98 @@ static inline uint8_t flash_send_recv(uint8_t data) {
 
 #define FLASH_READ_USE_DMA 1
 
-uint32_t flash_get_id(int flash_sel) {
-	int id=0;
+uint32_t flash_get_id(int flash_sel)
+{
+	int id = 0;
 	flash_start_xfer(flash_sel);
 	flash_send_recv(CMD_GETID);
-	id=flash_send_recv(0)<<16;
-	id|=flash_send_recv(0)<<8;
-	id|=flash_send_recv(0);
+	id = flash_send_recv(0) << 16;
+	id |= flash_send_recv(0) << 8;
+	id |= flash_send_recv(0);
 	flash_end_xfer();
 	return id;
 }
 
-uint64_t flash_get_uid(int flash_sel) {
+uint64_t flash_get_uid(int flash_sel)
+{
 	uint64_t uid;
 	flash_start_xfer(flash_sel);
 	flash_send_recv(CMD_GETUID);
-	for (int i=0; i<4; i++) flash_send_recv(0);
-	for (int i=0; i<8; i++) {
-		uid<<=8;
-		uid|=flash_send_recv(0);
+	for (int i = 0; i < 4; i++)
+		flash_send_recv(0);
+	for (int i = 0; i < 8; i++)
+	{
+		uid <<= 8;
+		uid |= flash_send_recv(0);
 	}
 	flash_end_xfer();
 	return uid;
 }
 
-
-void flash_read(int flash_sel, uint32_t addr, uint8_t *buff, int len) {
+void flash_read(int flash_sel, uint32_t addr, uint8_t *buff, int len)
+{
 #if FLASH_READ_USE_DMA
 	//Use DMA
-	MISC_REG(MISC_FLASH_SEL_REG)=(flash_sel==0)?MISC_FLASH_SEL_INTFLASH:MISC_FLASH_SEL_CARTFLASH;
-	while (len>0) {
+	MISC_REG(MISC_FLASH_SEL_REG) = (flash_sel == 0) ? MISC_FLASH_SEL_INTFLASH : MISC_FLASH_SEL_CARTFLASH;
+	while (len > 0)
+	{
 		//Transfer max 512 words at a time
-		int xfer_len=(len+3)/4;
-		if (xfer_len>512) xfer_len=512;
-		MISC_REG(MISC_FLASH_DMAADDR)=(uint32_t)buff;
-		MISC_REG(MISC_FLASH_RDADDR)=addr;
-		MISC_REG(MISC_FLASH_DMALEN)=xfer_len; //also starts xfer
+		int xfer_len = (len + 3) / 4;
+		if (xfer_len > 512)
+			xfer_len = 512;
+		MISC_REG(MISC_FLASH_DMAADDR) = (uint32_t)buff;
+		MISC_REG(MISC_FLASH_RDADDR) = addr;
+		MISC_REG(MISC_FLASH_DMALEN) = xfer_len; //also starts xfer
 		//wait till xfer is done
-		while((MISC_REG(MISC_FLASH_CTL_REG) & MISC_FLASH_CTL_DMADONE)==0);
+		while ((MISC_REG(MISC_FLASH_CTL_REG) & MISC_FLASH_CTL_DMADONE) == 0)
+			;
 		//subtract work done
-		len-=xfer_len*4;
-		buff+=xfer_len*4;
-		addr+=xfer_len*4;
+		len -= xfer_len * 4;
+		buff += xfer_len * 4;
+		addr += xfer_len * 4;
 	}
 #else
 	//Use manual SPI reads
 	flash_start_xfer(flash_sel);
 	flash_send_recv(CMD_FASTREAD);
-	flash_send_recv(addr>>16);
-	flash_send_recv(addr>>8);
+	flash_send_recv(addr >> 16);
+	flash_send_recv(addr >> 8);
 	flash_send_recv(addr);
 	flash_send_recv(0); //dummy
-	for (int i=0; i<len; i++) {
-		buff[i]=flash_send_recv(0);
+	for (int i = 0; i < len; i++)
+	{
+		buff[i] = flash_send_recv(0);
 	}
 	flash_end_xfer();
 #endif
 }
 
-uint8_t flash_read_status(int flash_sel, int reg) {
-	uint8_t regcmd[3]={CMD_READSR1, CMD_READSR2, CMD_READSR3};
-	if (reg<1 || reg>3) return 0xff;
+uint8_t flash_read_status(int flash_sel, int reg)
+{
+	uint8_t regcmd[3] = {CMD_READSR1, CMD_READSR2, CMD_READSR3};
+	if (reg < 1 || reg > 3)
+		return 0xff;
 	flash_start_xfer(flash_sel);
-	flash_send_recv(regcmd[reg-1]);
-	uint8_t r=flash_send_recv(0);
+	flash_send_recv(regcmd[reg - 1]);
+	uint8_t r = flash_send_recv(0);
 	flash_end_xfer();
 	return r;
 }
 
-bool flash_write_status(int flash_sel, int reg) {
-	uint8_t regcmd[3]={CMD_WRITESR1, CMD_WRITESR2, CMD_WRITESR3};
-	if (reg<1 || reg>3) return false;
+bool flash_write_status(int flash_sel, int reg)
+{
+	uint8_t regcmd[3] = {CMD_WRITESR1, CMD_WRITESR2, CMD_WRITESR3};
+	if (reg < 1 || reg > 3)
+		return false;
 	flash_start_xfer(flash_sel);
-	flash_send_recv(regcmd[reg-1]);
-	uint8_t r=flash_send_recv(0);
+	flash_send_recv(regcmd[reg - 1]);
+	uint8_t r = flash_send_recv(0);
 	flash_end_xfer();
 	return true;
 }
 
-
-bool flash_wake(int flash_sel) {
+bool flash_wake(int flash_sel)
+{
 	flash_start_xfer(flash_sel);
 	flash_send_recv(CMD_WAKE);
 	flash_end_xfer();
